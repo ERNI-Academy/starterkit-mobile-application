0c88062b50d828af9698c9991400afc2c9d7db4b
Oscar <oscar.vaquero@betterask.erni>
2022-01-28 12:09:13 +0100

    👷Add Build scripts #8

---
+++ scripts/CrossPlatform/Xamarin/UwpInstaller/Install-CertificateAndSideLoad.ps1
@@ -0,0 +1,15 @@
+﻿param(
+    [string]$packageName = $null,
+    [switch]$RunAsAdmin
+)
+if($RunAsAdmin){
+    $RelaunchArgs = '-ExecutionPolicy Bypass -file "' + "$PSScriptRoot\Install-CertificateAndSideLoad.ps1" + '"' + " $packageName"
+    
+    $AdminProcess = Start-Process "powershell.exe" -Verb RunAs -WorkingDirectory $PSScriptRoot -ArgumentList $RelaunchArgs -Wait
+    
+    exit 0
+}
+else{
+    certutil.exe -addstore TrustedPeople "$PSScriptRoot\$packageName.cer"
+    set-itemproperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowAllTrustedApps -Value 1 -Verbose    
+}

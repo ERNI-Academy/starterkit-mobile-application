acdb7135e5bd4849eb2a8ef12356b195fd6a1604
Robertcs8 <robertcluetserrano@gmail.com>
2022-06-02 11:23:50 +0200

    🎨 Improve viewmodels injections and remove unused resources.

--- src/CrossPlatform/MAUI/Erni.Mobile.MAUI/Views/ItemDetailPage.xaml.cs
+++ src/CrossPlatform/MAUI/Erni.Mobile.MAUI/Views/ItemDetailPage.xaml.cs
@@ -1,14 +1,12 @@
-using Erni.Mobile.MAUI.Services.Configuration;
-using Erni.Mobile.MAUI.Services.Logging;
 using Erni.Mobile.MAUI.ViewModels;
 
 namespace Erni.Mobile.MAUI.Views;
 
 public partial class ItemDetailPage : ContentPage
 {
-	public ItemDetailPage(ILoggingService loggingService, IApplicationSettingsService applicationSettingsService)
+	public ItemDetailPage(ItemDetailViewModel itemDetailViewModel)
 	{
 		InitializeComponent();
-		BindingContext = new ItemDetailViewModel(loggingService, applicationSettingsService);
+		BindingContext = itemDetailViewModel;
 	}
 }
